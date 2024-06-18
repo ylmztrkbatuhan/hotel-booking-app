24c4661650a86784cb82572136edbbe56d52f830
oboy21 <oboy11b@gmail.com>
2024-06-18 11:16:35 +0300

    method of reservation ticket

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
     def __init__(self,hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
     def book(self):
         """Book a hotel by changing its availability to no"""
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
@@ -20,9 +21,13 @@ class Hotel:
 class ReservationTicket:
 
     def __init__(self,customer_name, hotel_object):
-        pass
+         self.customer_name = customer_name
+         self.hotel = hotel_object
     def generate(self):
-       pass
+      content =f"""Thank you for your reservation! Here are you booking data:
+                Name: {self.customer_name}
+                Hotel name: {self.hotel.name}"""
+      return content
 
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
@@ -31,7 +36,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.generate())
 
 else:
